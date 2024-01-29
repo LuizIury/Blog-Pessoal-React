5bdc3ea273fecca38e71a7eb932625967afbdadc
Luiz Iury <luiziury_ios@hotmail.com>
2024-01-29 12:17:15 -0300

    CRUD de Postagens

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,4 +1,7 @@
 import homeLogo from '../../assets/home.png'
+import ListaPostagens from '../../components/postagens/listaPostagem/ListaPostagem';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
 
 
 function Home() {
@@ -11,9 +14,9 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
   
             <div className="flex justify-center ">
@@ -22,7 +25,7 @@ function Home() {
             </div>
           </div>
         </div>
-      
+        <ListaPostagens />
       </>
     );
 }
